488d74a40af5969f61a4a5dbcd4d3bd1460c8ad0
muhammadafiqzakaria <145213831+muhammadafiqzakaria@users.noreply.github.com>
2025-10-29 22:59:15 +0800

    progressing on FCM implementation

--- easy_parcel_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ easy_parcel_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import firebase_core
+import firebase_messaging
 import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,6 +15,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
